8b74daafddeef2d5108687c6eae03875c8ac5434
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-19 04:54:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,16 @@
 class Dog 
   
  
-  def initialize(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+ def initialize(name)
+    @name = name
   end
  
   def name
-    @this_dogs_name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
   end
   
   def breed=(dogs_breed)
